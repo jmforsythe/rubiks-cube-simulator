ab33f099eede3f0980422e6635dee063736214e7
fozzzyyy <jonathanmforsythe@gmail.com>
2018-03-04 21:33:08 +0000

    Added comments

--- net_display.py
+++ net_display.py
@@ -2,27 +2,36 @@ import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QPushButton, QFrame
 from PyQt5.QtGui import QColor
 
+# Default width of facelet squares (in pixels) for drawing the window
 width = 60
 
 
+# Define the class that will define our program window, inheriting from the QWidget class
 class NetDisplay(QWidget):
     def __init__(self, facelet_string):
+        # Calls the constructor of the QWidget class
         super().__init__()
 
+        # Initialises window while sanitising the facelet string by removing spaces
         self.init_ui(facelet_string.replace(" ", ""))
 
     def init_ui(self, facelet_string):
+        # Initialises a grid, and sets the layout of the program window to grid
         grid = QGridLayout()
         self.setLayout(grid)
 
+        # Inserts QFrames into the necessary grid positions in order to display the cube as a 2D net
         positions = [(i, j) for i in range(9) for j in range(12)]
         for position in positions:
             if position[0] in range (3, 6) or position[1] in range(3, 6):
                 frame = QFrame(self)
                 frame.col = QColor(235, 235, 235)
-                frame.setStyleSheet("background-color: %s; margin:10px; border:3px solid rgb(20,20,20); " % frame.col.name())
+                frame.setStyleSheet("background-color: %s; margin:10px; border:3px solid rgb(20,20,20); "
+                                    % frame.col.name())
                 grid.addWidget(frame, *position)
 
+        # This array converts the string format generated from the facelet representation into something that
+        # can be displayed on the grid, as the grid is numbered row by row, left to right
         string_converter = [0, 1, 2,
                             3, 4, 5,
                             6, 7, 8,
@@ -34,6 +43,7 @@ class NetDisplay(QWidget):
                             33, 34, 35]
         facelet_string = [facelet_string[i] for i in string_converter]
 
+        # This reads the converted facelet string, and colours in each facelet square accordingly
         for facelet in range(54):
             frame = grid.itemAt(facelet).widget()
             if facelet_string[facelet] == "U":
@@ -50,7 +60,8 @@ class NetDisplay(QWidget):
                 frame.col = QColor(255, 165, 0)  # orange
             frame.setStyleSheet("background-color: %s; margin:0px; border:2px solid rgb(20,20,20);" % frame.col.name())
 
-        self.setGeometry(300, 300, (width + 10) * 12 + 100, (width + 10) * 9 + 100)
+        # Defines the size of the window and displays it
+        self.setGeometry(300, 300, width * 12 + 100, width * 9 + 100)
         self.show()
 
 
